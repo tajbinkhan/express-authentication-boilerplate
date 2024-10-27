f803a9e54e0ee120dd93bcbd114cf77de29e9afc
Tajbin Khan <tajbink@gmail.com>
2024-10-27 20:15:03 +0600

    Remove unnecessary Drizzle migration files and table creation script

--- .drizzle/migrations/0000_puzzling_spectrum.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "todo" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"title" varchar(255) NOT NULL,
-	"description" text NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
-);
