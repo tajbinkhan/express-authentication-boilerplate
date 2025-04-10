af8a267b69e0961860e9b36264c709d920a6c806
Tajbin Khan <tajbink@gmail.com>
2025-04-10 23:17:20 +0600

    Update tsup configuration to disable sourcemaps and ensure consistent output format

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["./src", "drizzle.config.ts"],
 	splitting: false,
-	sourcemap: true,
+	sourcemap: false,
 	clean: true,
-	define: { "process.env.NODE_ENV": "'production'" }
+	define: { "process.env.NODE_ENV": "'production'" },
+	format: ["esm"],
+	outExtension: () => ({
+		js: `.js`
+	})
 });
