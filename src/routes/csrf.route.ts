b7ae263ccd2f60f3c51dd10d6aba171767d9d130
Tajbin Khan <tajbink@gmail.com>
2025-05-27 21:31:04 +0600

    refactor: rename CSRF token generation function and update related imports
    - Changed the function name from `generateToken` to `generateCsrfToken` for clarity.
    - Updated the CSRF route to use the new function name.
    - Adjusted the export statement in the CSRF utility file to reflect the new function name.
    - Enhanced the CSRF configuration by adding `getSessionIdentifier` for improved security.

--- src/routes/csrf.route.ts
+++ src/routes/csrf.route.ts
@@ -1,13 +1,13 @@
 import express, { Router } from "express";
 
-import { generateToken } from "@/utils/csrf";
+import { generateCsrfToken } from "@/utils/csrf";
 import { ApiResponse } from "@/utils/serviceApi";
 
 export const csrfRouter: Router = (() => {
 	const router = express.Router();
 
 	router.get("", (req, res) => {
-		new ApiResponse(res).successResponse("CSRF token generated", generateToken(req, res));
+		new ApiResponse(res).successResponse("CSRF token generated", generateCsrfToken(req, res));
 	});
 
 	return router;
--- src/utils/csrf.ts
+++ src/utils/csrf.ts
@@ -3,27 +3,24 @@ import { doubleCsrf } from "csrf-csrf";
 import { csrfTimeout } from "@/core/constants";
 import AppHelpers from "@/utils/appHelpers";
 
-const {
-	generateToken, // Use this in your routes to generate tokens
-	validateRequest, // Use this as middleware to validate tokens
-	doubleCsrfProtection, // This is the express middleware
-	invalidCsrfTokenError // This is the error thrown when the token is invalid
-} = doubleCsrf({
-	getSecret: () => process.env.SECRET, // Secret for generating tokens
-	cookieName: "csrf-token", // Cookie name for the token
-	cookieOptions: {
-		sameSite: AppHelpers.sameSiteCookieConfig().sameSite,
-		secure: AppHelpers.sameSiteCookieConfig().secure,
-		maxAge: csrfTimeout,
-		...(AppHelpers.sameSiteCookieConfig().domain && {
-			domain: AppHelpers.sameSiteCookieConfig().domain
-		})
-	},
-	size: 32, // The size of the generated tokens
-	errorConfig: {
-		message: "Invalid CSRF token"
-	},
-	getTokenFromRequest: req => req.headers["x-csrf-token"] // Get the token from the request
-});
+const { generateCsrfToken, validateRequest, doubleCsrfProtection, invalidCsrfTokenError } =
+	doubleCsrf({
+		getSecret: () => process.env.SECRET,
+		getSessionIdentifier: () => process.env.SECRET,
+		cookieName: "csrf-token",
+		cookieOptions: {
+			maxAge: csrfTimeout,
+			sameSite: AppHelpers.sameSiteCookieConfig().sameSite,
+			secure: AppHelpers.sameSiteCookieConfig().secure,
+			...(AppHelpers.sameSiteCookieConfig().domain && {
+				domain: AppHelpers.sameSiteCookieConfig().domain
+			})
+		},
+		size: 32,
+		errorConfig: {
+			message: "Invalid CSRF token"
+		},
+		getCsrfTokenFromRequest: req => req.headers["x-csrf-token"]
+	});
 
-export { doubleCsrfProtection, generateToken, invalidCsrfTokenError, validateRequest };
+export { doubleCsrfProtection, generateCsrfToken, invalidCsrfTokenError, validateRequest };
